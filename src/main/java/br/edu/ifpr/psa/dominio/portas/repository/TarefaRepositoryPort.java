369b258e7851fb1e298a54a73e439ccf22736040
IgorLuigi <igor.luigi@hotmail.com>
2022-09-08 20:07:20 -0300

    Correções

--- src/main/java/br/edu/ifpr/psa/dominio/portas/repository/TarefaRepositoryPort.java
+++ src/main/java/br/edu/ifpr/psa/dominio/portas/repository/TarefaRepositoryPort.java
@@ -1,3 +1,8 @@
+package br.edu.ifpr.psa.dominio.portas.repository;
+
+import java.util.List;
+import br.edu.ifpr.psa.dominio.Tarefa;
+
 public interface TarefaRepositoryPort {
     List<Tarefa> buscarTodos();
 
